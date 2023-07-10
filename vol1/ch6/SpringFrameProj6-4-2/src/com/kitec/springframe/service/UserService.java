585012cfa203e2681010c29560eef73e463ff41e
intheeast <intheeast0725@gmail.com>
2023-07-10 17:28:48 +0900

    renew ch6

--- vol1/ch6/SpringFrameProj6-4-2/src/com/kitec/springframe/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.kitec.springframe.service;
-
-import com.kitec.springframe.domain.User;
-
-public interface UserService {
-	void add(User user);
-	void upgradeLevels();
-}
-
--- vol1/ch6/SpringFrameProj6-5-3/src/com/kitec/learningtest/spring/pointcut/Bean.java
+++
@@ -1,6 +0,0 @@
-package com.kitec.learningtest.spring.pointcut;
-
-public class Bean {
-	public void method() throws RuntimeException {
-	}
-}
